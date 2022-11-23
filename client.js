a88c2a4e0696620b2daa6a861ad120b404266ebe
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-23 14:39:39 -0800

    added connect exported as object

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require("net");
+const connect = function () {
+    const conn = net.createConnection({
+      host: "165.227.47.243",
+      port:50541
+    });
+    conn.on("connect", () => {
+      console.log("you ded cuz you idled");
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  
+    return conn;
+  };
+  module.exports = {connect};
