540785e24a65b93a0b5cea03a247175889bd1d72
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-23 19:35:11 -0800

    edited

---
+++ constants.js
@@ -0,0 +1,22 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const MOVE_UP = 'w';
+const MOVE_LEFT = 'a';
+const MOVE_DOWN = 's';
+const MOVE_RIGHT = 'd';
+const NAME = 'MOS';
+const MESSAGES =
+  {x: 'Say: Watch Out!',
+    y: 'Say: over my dead body!',
+    z: 'Say: O man!'};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  NAME,
+  MESSAGES
+};
